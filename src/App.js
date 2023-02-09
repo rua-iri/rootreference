162555528826e59b239b1cee3a5a1110994c38e2
rua-iri <rory.mcguigan@protonmail.com>
2023-02-09 07:26:39 +0000

    implement api query and create basic page structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
-
+import DictionaryResult from "./components/DictionaryResult";
 
 
 function App() {
 
+  const [lisanData, setLisanData] = React.useState({});
+
+
   // get the query parameters to use for the api
-  const queryParameters = new URLSearchParams(window.location.search)
+  const queryParameters = new URLSearchParams(window.location.search);
+  let rootWord = queryParameters.get("q");
 
-  console.log(queryParameters.get("q"))
+  // if root is only two letters then its last letter 
+  // should be doubled e.g. قل => قلل 
+  if(rootWord.length===2) {
+    rootWord = rootWord + rootWord[1];
+  }
 
 
-  // TODO query api for the root
-  // then use the results returned create new DictionaryResult elements
+  const apiUrl = "https://isd5kzb9si.execute-api.us-east-1.amazonaws.com/dictionarylookup?root=" + rootWord;
+
+  function checkDictionary() {
+
+    axios.get(apiUrl).then((response) => {
+      setLisanData(response.data.Item);
+    })
+
+  }
+
+  React.useState(() => {
+    checkDictionary();
+  }, [])
+
 
   // TODO create search box to appear if no query parameters are given
 
 
   return (
-    <div className="App">
+    <div className="App gimmie-outline">
       Arabic Root Reference
-      <br />
-      {queryParameters.get("q")}
+      <div className='gimmie-outline' id='root-section'>
+        {rootWord}
+      </div>
+
+      <div className='break-segment gimmie-outline'></div>
+
+
+      <div>
+        <DictionaryResult entryData={lisanData} />
+      </div>
     </div>
   );
 }
--- src/components/DictionaryResult.js
+++ src/components/DictionaryResult.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-export default function DictionaryResult() {
+export default function DictionaryResult(props) {
+
+    // TODO format this text to make it easier to read
+
+    // TODO add onclick event so that the dictionary entry can be 
+    // collapsed and expanded maybe even hide some of especially long
+    // entries until users click on them
+
 
     return (
-        <div>
-            
+        <div className="dict-result gimmie-outline">
+            <div className="dict-title gimmie-outline">
+                Lisan Al Arab
+            </div>
+            <div className="dict-entry gimmie-outline">
+                {props.entryData ? props.entryData.text : ""}
+            </div>
         </div>
     )
 }
